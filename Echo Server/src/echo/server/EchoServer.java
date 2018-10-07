ad7c2025de8e4dfefe0affaeea416def2dcebcb8
Sam <campbellsam97@gmail.com>
2018-10-07 14:05:39 -0400

    Server runs continously + custom IPs
    The server will keep looking for another client connection after one is closed.
    The client can specify a custom IP that the server is running on

--- Echo Server/src/echo/server/EchoServer.java
+++ Echo Server/src/echo/server/EchoServer.java
@@ -26,36 +26,38 @@ public class EchoServer {
             InputStream in = null;
             OutputStream out = null;
 
-            byte[] clientInData = new byte[8000]; //A large size is used and will be truncated into another array for output
+            while (true) {
+                byte[] clientInData = new byte[8000]; //A large size is used and will be truncated into another array for output
 
-            System.out.println("Waiting for a connection");
-            
-            while (connected == false) { //Listen for connections
-                client = sock.accept();
-                if (client != null) {
-                    System.out.println("Connection established");
-                    connected = true;
-                }
-            }
-            while (connected == true) { //Read and send the echo request from the client
-                in = client.getInputStream(); //Get input stream from client
-                out = client.getOutputStream(); //Create byte stream to client
-                byte[] clientOutData;
-                
-                int nBytesRead = in.read(clientInData); //Track the number of bytes read while reading them
-                
-                if (nBytesRead != -1) { //Ensure the socket connection is still established
-                    clientOutData = Arrays.copyOf(clientInData, nBytesRead); //Resize the original input byte array
-                } else {
-                    connected = false;
-                    break;
-                }
+                System.out.println("Waiting for a connection.");
 
-                out.write(clientOutData); //Write bytes to client
-                out.flush();
+                while (connected == false) { //Listen for connections
+                    client = sock.accept();
+                    if (client != null) {
+                        System.out.println("Connection established");
+                        connected = true;
+                    }
+                }
+                while (connected == true) { //Read and send the echo request from the client
+                    in = client.getInputStream(); //Get input stream from client
+                    out = client.getOutputStream(); //Create byte stream to client
+                    byte[] clientOutData;
+
+                    int nBytesRead = in.read(clientInData); //Track the number of bytes read while reading them
+
+                    if (nBytesRead != -1) { //Ensure the socket connection is still established
+                        clientOutData = Arrays.copyOf(clientInData, nBytesRead); //Resize the original input byte array
+                    } else {
+                        connected = false;
+                        break;
+                    }
+
+                    out.write(clientOutData); //Write bytes to client
+                    out.flush();
+                }
+                client.close(); //Close client connection if -1 is returned from the input stream
+                System.out.println("Connection closed.");
             }
-            client.close(); //Close client connection if -1 is returned from the input stream
-            System.out.println("Connection closed. Terminating.");
         } catch (IOException ioe) {
             System.err.println(ioe);
         }
